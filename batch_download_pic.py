3e3318291b169bb7df9c3f80a5eb734c419395fe
田杰 <jeff.tian@outlook.com>
2016-09-29 14:38:19 +0800

    Download HD pictures at once.

---
+++ batch_download_pic.py
@@ -0,0 +1,42 @@
+from urlparse import urlsplit
+from os.path import basename
+import urllib2
+import re
+import requests
+import os
+import json
+
+url = 'https://zh.airbnb.com/rooms/14047326'
+
+dir_name = 'houses/'
+if not os.path.exists(dir_name):
+    os.mkdir(dir_name)
+
+offset = 0
+f = open('./houses/source.txt')
+url_content = f.read()
+answers = re.findall('z2\.muscache\.com\/ac\/pictures\/([^\.]+)\.jpg', url_content)
+img_count = 0
+
+print len(answers)
+
+while offset < len(answers):
+    post_url = "https://z2.muscache.com/ac/pictures/" + answers[offset] + ".jpg"
+    print post_url
+    try:
+        img_data = urllib2.urlopen(post_url).read()
+        file_name = basename(urlsplit(post_url)[2])
+        output = open(dir_name + file_name, 'wb')
+        output.write(img_data)
+        output.close()
+        print "Saved {} images".format(img_count)
+        # print "Saved {} images at url {}".format(img_count, img_url)
+        img_count += 1
+    except KeyboardInterrupt:
+        print "Exit on user prompt"
+        exit(1)
+    except:
+        pass
+    finally:
+        offset += 1
+        print "Page down - offset {}".format(offset)
